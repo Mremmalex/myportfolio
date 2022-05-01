772b0fdf2f4b42f2e0b3af511552e4be3ae7bb37
mremmalex <mremmalex2@gmail.com>
2022-05-01 22:49:11 +0100

    making the mobile responsive

--- src/components/layout/about/AboutSection.tsx
+++ src/components/layout/about/AboutSection.tsx
@@ -13,24 +13,24 @@ import postgress from "../../../assests/postgress.png";
 function AboutSection() {
 	return (
 		<section className={style.about_section}>
+			<div className={style.about_head}>
+				<h1 className="">About Me</h1>
+				<p>
+					I'm a fullstack web and mobile developer, i work with alot of
+					technologies to bring your ideas to life
+				</p>
+				<h4>tools i use includes the following:</h4>
+			</div>
 			<div className={style.container}>
-				<div className={style.about_head}>
-					<h1 className="">About Me</h1>
-					<p>
-						I'm a fullstack web and mobile developer, i work with alot of
-						technologies to bring your ideas to life
-					</p>
-					<h4>tools i use includes the following:</h4>
-					<div className={style.skill}>
-						<Card image={react} skill="Reactjs" />
-						<Card image={react} skill="React Native" />
-						<Card image={next} skill="Nextjs" />
-						<Card image={typescript} skill="Typescript" />
-						<Card image={express} skill="Express Js" />
-						<Card image={python} skill="Python" />
-						<Card image={postgress} skill="Postgress" />
-						<Card image={mongodb} skill="Mongo Database" />
-					</div>
+				<div className={style.skill}>
+					<Card image={react} skill="Reactjs" />
+					<Card image={react} skill="React Native" />
+					<Card image={next} skill="Nextjs" />
+					<Card image={typescript} skill="Typescript" />
+					<Card image={express} skill="Express Js" />
+					<Card image={python} skill="Python" />
+					<Card image={postgress} skill="Postgress" />
+					<Card image={mongodb} skill="Mongo Database" />
 				</div>
 			</div>
 		</section>
