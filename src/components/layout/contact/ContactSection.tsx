83aa9e2f1e0a623923e2410184d8fbca27a3a5c2
mremmalex <mremmalex2@gmail.com>
2022-05-06 08:38:37 +0100

    remove the iframe

--- src/components/layout/contact/ContactSection.tsx
+++ src/components/layout/contact/ContactSection.tsx
@@ -19,13 +19,7 @@ function ContactSection() {
 					</div>
 
 					<div className={style.contact_right}>
-						<iframe
-							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.8651985793103!2d7.3570424144561395!3d5.1254113392563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10429966f24c317f%3A0xca084cdf8a33f168!2s10%20Calabar%20Street%2C%20Aba%20450211%2C%20Aba!5e0!3m2!1sen!2sng!4v1651444467912!5m2!1sen!2sng"
-							width="600"
-							height="450"
-							allowfullscreen=""
-							loading="lazy"
-							referrerpolicy="no-referrer-when-downgrade"></iframe>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.8651985793103!2d7.3570424144561395!3d5.1254113392563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10429966f24c317f%3A0xca084cdf8a33f168!2s10%20Calabar%20Street%2C%20Aba%20450211%2C%20Aba!5e0!3m2!1sen!2sng!4v1651444467912!5m2!1sen!2sng"></iframe>
 					</div>
 				</div>
 			</Container>
